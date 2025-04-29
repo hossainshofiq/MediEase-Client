93536f619121bb23cdbf58f5bec7399e79f1bc3e
hossainshofiq <ah731710@gmail.com>
2025-04-30 01:17:20 +0600

    Changed Section Title Color in every page for better user experience

--- src/Components/SectionTitle.jsx
+++ src/Components/SectionTitle.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const SectionTitle = ({heading , subHeading}) => {
     return (
         <div className='text-center rounded-md border p-2 lg:w-1/2 mx-auto'>
-            <p className='text-xl md:text-2xl lg:text-3xl font-semibold text-red-600 pb-2 border-b uppercase'>{heading}</p>
-            <h3 className='text-green-600'>{subHeading}</h3>
+            <p className='text-xl md:text-2xl lg:text-3xl font-semibold text-primary pb-2 border-b uppercase'>{heading}</p>
+            <h3 className='text-success'>{subHeading}</h3>
         </div>
     );
 };
